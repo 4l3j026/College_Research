76e70cf84479fb44368fe7afb1688c02233ee9fe
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-04-10 18:15:20 -0500

    Create IoT template test Wednesday, April 10, 2024

---
+++ IoT_Test.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: Alejo
+ *
+ * Created on April 10, 2024, 6:09 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+
+    while (1) {
+
+        
+
+    }
+    return;
+}
